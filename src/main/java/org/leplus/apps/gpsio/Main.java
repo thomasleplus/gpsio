10c2afdb3928981f14142446360edfad92489987
Thomas Leplus <thomas@leplus.org>
2012-08-26 18:05:22 +0200

    Init

---
+++ src/main/java/org/leplus/apps/gpsio/Main.java
@@ -0,0 +1,16 @@
+/**
+ * 
+ */
+package org.leplus.apps.gpsio;
+
+/**
+ * @author thomas
+ *
+ */
+public class Main {
+
+	public static void main(String[] args) {
+		// Nothing to see here
+	}
+
+}
