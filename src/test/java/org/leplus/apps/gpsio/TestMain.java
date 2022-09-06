334ebc2322cfce4092a37e051bb2c7624aa5ec8e
Thomas Leplus <thomasleplus@users.noreply.github.com>
2022-09-05 17:05:37 -0700

    Test

---
+++ src/test/java/org/leplus/apps/gpsio/TestMain.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package org.leplus.apps.gpsio;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * @author thomas
+ *
+ */
+class TestMain {
+
+    @Test
+    void test() {
+        Main.main(null);
+    }
+
+}
